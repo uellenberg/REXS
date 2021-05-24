8503b9f6fe3d68b9889e9dd02d074dc411a9dd9b
uellenberg <JonahUellenberg@hotmail.com>
2021-05-23 20:48:15 -0700

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,8 @@ module.exports = {
     entry: "./dist/esm/index.js",
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "decimalsystem.min.js",
-        library: "decimalsystem"
+        filename: "REXS.min.js",
+        library: "REXS"
     },
     mode: "production"
 };
